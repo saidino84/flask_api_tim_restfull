61842b513cd2564014a4dfb37d8f7418f2d82e66
saidino83 <tylorguy2018@gmail.com>
2021-06-12 09:28:19 +0200

    modificando aport e removendo o parametro do debug na instacia do app.run(port=os.environ.get(port,5000))

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
+import os
 from app import create_app
+
+port=int(os.environ.get('PORT',5000))
 
 if __name__=='__main__':
     app =create_app()
-    app.run(debug=True,host='0.0.0.0')
+    app.run(host='0.0.0.0',port=port)
 else:
     gunicorn_app=create_app()
